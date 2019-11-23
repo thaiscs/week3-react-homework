daf53ba19826393905cfcf6d5c2f9a6cf8ff2146
thaiscs <thais.advj@gmail.com>
2019-11-23 13:37:09 +0100

    completed step 4

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 
 class Quote extends Component {
+  state = {
+    color: "black",
+    textDecoration: ""
+  };
+
+  dislikedQuote = (color, textDecoration) => {
+    // console.log("TESTING");
+    this.setState({ color: color, textDecoration: textDecoration });
+  };
+
+  likedQuote = color => {
+    // console.log("TESTING");
+    this.setState({ color: color });
+  };
+
   render() {
     return (
       <div className="quotes-presentation">
-        {this.props.quoteText}
+        <div
+          style={{
+            color: this.state.color,
+            textDecorationLine: this.state.textDecoration
+          }}
+        >
+          {this.props.quoteText}
+        </div>
         <p>
           <strong>By: {this.props.quoteAuthor}</strong>
         </p>
+        <button onClick={() => this.likedQuote("pink")}>:)</button>
+        <button onClick={() => this.dislikedQuote("red", "line-through")}>
+          :(
+        </button>
       </div>
     );
   }
